e54a24d20bae4356d3a89f761c626ed0726ec0a9
Anoop Bhat <noopy.g.bhat@gmail.com>
2020-02-09 20:35:34 -0500

    Corrected comparison node

--- reusnake_control/src/estimation/comparison_node.cpp
+++ reusnake_control/src/estimation/comparison_node.cpp
@@ -22,28 +22,45 @@ int main(int argc, char **argv) {
   ros::Rate r(50); // 50 hz
 
   bool first = true;
-  Quaterniond initial_err = Quaterniond::Identity();
+  Quaterniond vins_initial_pose;
+  Quaterniond estimator_initial_pose;
   while (ros::ok()) {
     try {
-      tf::StampedTransform err;
-      tf_listener.lookupTransform("link0", "body",  
-                                  ros::Time(0), err);
-      Quaterniond err_q(err.getRotation().w(),
-                        err.getRotation().x(),
-                        err.getRotation().y(),
-                        err.getRotation().z());
-      err_q = initial_err.conjugate()*err_q;
+      tf::StampedTransform vins;
+      tf::StampedTransform estimator;
+      tf_listener.lookupTransform("body", "world",  
+                                  ros::Time(0), vins);
+      tf_listener.lookupTransform("link0", "world",  
+                                  ros::Time(0), vins);
+      
+      Quaterniond vins_pose;
+      Quaterniond estimator_pose;
+
+      vins_pose.w() = vins.getRotation().w();
+      vins_pose.x() = vins.getRotation().x(),
+      vins_pose.y() = vins.getRotation().y();
+      vins_pose.z() = vins.getRotation().z(),
+      
+      estimator_pose.w() = estimator.getRotation().w();
+      estimator_pose.x() = estimator.getRotation().x(),
+      estimator_pose.y() = estimator.getRotation().y();
+      estimator_pose.z() = estimator.getRotation().z();
 
       if (first) {
         first = false;
-        initial_err = err_q;
+        vins_initial_pose = vins_pose;
+        estimator_initial_pose = estimator_pose;
       }
+  
+      Quaterniond vins_disp = vins_initial_pose.conjugate()*vins_pose;
+      Quaterniond estimator_disp = estimator_initial_pose.conjugate()*estimator_pose;
 
+      Quaterniond err_q = vins_disp.conjugate()*estimator_disp;
       Vector3d ypr = err_q.toRotationMatrix().eulerAngles(2, 1, 0);
      
-      Vector3d err_pos(err.getOrigin().x(),
-                       err.getOrigin().y(),
-                       err.getOrigin().z());
+      Vector3d err_pos(vins.getOrigin().x(),
+                       vins.getOrigin().y(),
+                       vins.getOrigin().z());
       if (err_pos.norm() < 10) {
         printf("Difference between VINS and VC estimator - Roll: %lf Pitch: %lf Yaw: %lf\n",
                ypr(2), ypr(1), ypr(0));
